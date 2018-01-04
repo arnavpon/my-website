f6d21a6018728c5eeb3e42f5d7cd9bcbbee28792
Arland Porter <arnavpon@gmail.com>
2018-01-04 14:00:29 -0500

    added copy

--- project/urls.py
+++ project/urls.py
@@ -14,7 +14,6 @@ urlpatterns = [
 
     url(r'^$', views.index),
     url(r'^health$', views.health),
-    url(r'^admin/', include(admin.site.urls)),
     path('grocery/', include('welcome.urls'))
 ]  # point the root urls.py -> the welcome urls.py module
 # include() allows referencing of other url config files (urls.py)
@@ -23,6 +22,9 @@ urlpatterns = [
 #  - route: a URL pattern, searches from TOP -> BOTTOM;
 #  - view: the view called when Django finds a matching pattern, sends a HttpRequest object as argument
 #  - name: allows you to unambiguously name the URL to refer to it from within templates
+
+# url(r'^admin/', include(admin.site.urls)),
+# django>=1.11,<1.12
 
 if settings.DEBUG:
     import debug_toolbar
